3a143809b9effc52da5330d24a5656abd15ceec8
hofstede-matheus <hofstede.matheus@gmail.com>
2024-05-16 13:59:24 -0300

    set initial state as default express app

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-module.exports.handler = serverless(app);
+// module.exports.handler = serverless(app);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
